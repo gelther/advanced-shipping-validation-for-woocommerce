e10f75b1afb441216cce08cfd2b65c5e79037143
gelther <lpw.sormano@gmail.com>
2016-01-31 13:35:00 +0100

    Create random.php

---
+++ random.php
@@ -0,0 +1,19 @@
+<?php
+
+function is_foo( $foo = '' ) {
+	if ( $foo == 'foo' ) {
+		return TRUE;
+	}
+	return FALSE;
+}
+
+function is_bar($bar = '') 
+{
+	if ($bar == 'bar') 
+	{
+		return true;
+	}
+	return false;
+}
+
+
