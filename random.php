66dd4da329efd935170ff070d2c4f29b70b7d2f1
gelther <lpw.sormano@gmail.com>
2016-01-31 13:36:08 +0100

    Code consistency changes for random.php

--- random.php
+++ random.php
@@ -2,18 +2,16 @@
 
 function is_foo( $foo = '' ) {
 	if ( $foo == 'foo' ) {
-		return TRUE;
-	}
-	return FALSE;
-}
-
-function is_bar($bar = '') 
-{
-	if ($bar == 'bar') 
-	{
 		return true;
 	}
 	return false;
 }
 
 
+function is_bar( $bar = '' ) {
+
+	if ( $bar == 'bar' ) {
+		return true;
+	}
+	return false;
+}
